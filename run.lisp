6d73c9b7f3d905d363a08f6bbe4b013cdd04b82e
Vsevolod Dyomkin <vseloved@gmail.com>
2016-12-04 11:42:52 +0200

    fix

--- run.lisp
+++ run.lisp
@@ -1,5 +1,6 @@
 (push :prod *features*)
 
 (push (merge-pathnames "../wiki-lang-detect/") asdf:*central-registry*)
+(push (merge-pathnames "../rutils/") asdf:*central-registry*)
 
 (ql:quickload :wiki-lang-detect)
